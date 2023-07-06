41735229614451de580955a484d09a0115cb87a0
OdaGuzman <odaguz.dev@gmail.com>
2023-07-06 15:08:50 -0700

    Dark mode 85% completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,28 @@
+'use client'
 import Landing from '@/components/Landing'
 import NavBar from '@/components/Navbar'
 import Projects from '@/components/Projects'
+
+import { useState, useEffect } from 'react';
+
 export default function Home() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  // Function to toggle between light and dark mode
+  const toggleTheme = () => {
+    setIsDarkMode((prevMode) => !prevMode);
+  };
+
+  // Set the initial theme based on user's system preference
+  useEffect(() => {
+    const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    setIsDarkMode(prefersDarkMode);
+  }, []);
+
+  // Update the HTML root element class based on the theme
+  useEffect(() => {
+    document.documentElement.classList.toggle('dark', isDarkMode);
+  }, [isDarkMode]);
   return (
     <>
       <NavBar />
--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -2,23 +2,23 @@
 import { useState, useEffect } from 'react';
 
 const ThemeToggle = () => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  // const [isDarkMode, setIsDarkMode] = useState(false);
 
-  // Function to toggle between light and dark mode
-  const toggleTheme = () => {
-    setIsDarkMode((prevMode) => !prevMode);
-  };
+  // // Function to toggle between light and dark mode
+  // const toggleTheme = () => {
+  //   setIsDarkMode((prevMode) => !prevMode);
+  // };
 
-  // Set the initial theme based on user's system preference
-  useEffect(() => {
-    const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-    setIsDarkMode(prefersDarkMode);
-  }, []);
+  // // Set the initial theme based on user's system preference
+  // useEffect(() => {
+  //   const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  //   setIsDarkMode(prefersDarkMode);
+  // }, []);
 
-  // Update the HTML root element class based on the theme
-  useEffect(() => {
-    document.documentElement.classList.toggle('dark', isDarkMode);
-  }, [isDarkMode]);
+  // // Update the HTML root element class based on the theme
+  // useEffect(() => {
+  //   document.documentElement.classList.toggle('dark', isDarkMode);
+  // }, [isDarkMode]);
 
   return (
     <button
