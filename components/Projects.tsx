899ad129ceede51ba63d74db67bff03394b0ed03
OdaGuzman <odaguz.dev@gmail.com>
2023-07-07 12:06:28 -0700

    Dark mode - 82%

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -5,11 +5,11 @@ import { ThemeToggleProps } from '../components/types';
 export default function Projects({ isDarkMode, toggleTheme }: ThemeToggleProps) {
   return (
     <div className="flex flex-col items-center pt-20 pb-40 font-montserrat" id="projects">
-      <div className="text-5xl mb-5 py-20">Projects</div>
+      <div className="text-4xl mb-5 py-20">Projects</div>
 
-      <div className="flex gap-72 lg:gap-40 flex-col lg:flex-row">
+      <div className="flex gap-72 lg:gap-80 flex-col lg:flex-row">
 
-        {/* Image 1 */}
+        {/* Image 1 - 1st Row*/}
         <div className="relative w-96 h-96">
           <div
             className="bg-white border-8 border-gray-300 p-5 flex justify-center items-center w-full h-full transition-colors ">
@@ -42,52 +42,8 @@ export default function Projects({ isDarkMode, toggleTheme }: ThemeToggleProps) 
           </div>
         </div>
 
-        {/* Image 2 */}
-        <div className="relative w-96 h-96">
-          <div className="bg-white border-8 border-gray-300 p-5 flex justify-center items-center w-full h-full transition-colors ">
-            <span className="text-black text-4xl">InStock 💰</span>
-            <a href="https://instock-app-example.com/">
-              <Image
-                src="/Instock.png"
-                alt="InStock"
-                className="absolute top-0 left-0 w-full h-full object-cover opacity-0 transition-opacity duration-300 hover:opacity-100"
-                width={384}
-                height={384}
-              />
-            </a>
-          </div>
-          <div className="w-full h-full lex flex-col justify-center items-center pt-4">
-            <p className="text-xl">InStock is a Full-Stack team collaboration with fellow colleagues to develop an Inventory Management System, where the user can add and remove items from the Inventory page and select from a list of Warehouses to modify the selected warehouse location. </p>
-            <ul className="flex items-center justify-between pt-4 text-sm">
 
-              <li>Jira</li><span className="text-purple-600 text-xl ml-1">|</span>
-              <li>React</li><span className="text-purple-600 text-xl ml-1">|</span>
-              <li>Knex</li><span className="text-purple-600 text-xl ml-1">|</span>
-              <li>Postman</li><span className="text-purple-600 text-xl ml-1">|</span>
-
-              <a href="https://github.com/OdaGuzman/InStockClient">
-                <Image
-                  src="/github-icon.png"
-                  alt="Github Repo Client"
-                  className="h-7 ml-6"
-                  width={28}
-                  height={28}
-                />
-              </a>
-              <a href="https://github.com/OdaGuzman/InStockServer">
-                <Image
-                  src="/github-icon.png"
-                  alt="Github Repo Server"
-                  className="h-7 ml-auto"
-                  width={28}
-                  height={28}
-                />
-              </a>
-            </ul>
-          </div>
-        </div>
-
-        {/* Image 3 */}
+        {/* Image 2 - 1st Row */}
         <div className="relative w-96 h-96">
           <div className="bg-white border-8 border-gray-300 p-5 flex justify-center items-center w-full h-full transition-colors ">
             <span className="text-black text-4xl">Wasabi Bytes 🍣</span>
@@ -130,12 +86,8 @@ export default function Projects({ isDarkMode, toggleTheme }: ThemeToggleProps) 
       </div>
 
 
-
-
-
-
-      {/* Second Row */}
-      <div className="flex gap-72 lg:gap-40 flex-col lg:flex-row mt-64">
+      {/* Image 1 - 2nd Row */}
+      <div className="flex gap-72 lg:gap-80 flex-col lg:flex-row mt-64">
 
         {/* Image 1 */}
         <div className="relative w-96 h-96">
@@ -167,8 +119,8 @@ export default function Projects({ isDarkMode, toggleTheme }: ThemeToggleProps) 
           </div>
         </div>
 
-        {/* Image 2 */}
-        {/* <div className="relative w-96 h-96">
+        {/* Image 2 - 2nd Row*/}
+        <div className="relative w-96 h-96">
           <div className="bg-white border-8 border-gray-300 p-5 flex justify-center items-center w-full h-full transition-colors ">
             <span className="text-black text-4xl">InStock 💰</span>
             <a href="https://instock-app-example.com/">
@@ -210,43 +162,7 @@ export default function Projects({ isDarkMode, toggleTheme }: ThemeToggleProps) 
               </a>
             </ul>
           </div>
-        </div> */}
-
-        {/* Image 3 */}
-        {/* <div className="relative w-96 h-96">
-          <div className="bg-white border-8 border-gray-300 p-5 flex justify-center items-center w-full h-full transition-colors ">
-            <span className="text-black text-4xl">Wasabi Bytes 🍣</span>
-            <a href="https://wasabi-bytes-app.com/">
-              <Image
-                src="/wasabi.png"
-                alt="Wasabi Bytes"
-                className="absolute top-0 left-0 w-full h-full object-cover opacity-0 transition-opacity duration-300 hover:opacity-100"
-                width={384}
-                height={384}
-              />
-            </a>
-          </div>
-          <div className="w-full h-full flex-col justify-center items-center pt-4">
-            <p className="text-xl">Wasabi Bytes is a full-stack project that showcases an interactive sushi menu. Users are able to create an account, log in,and place orders for their favorite sushi dishes.</p>
-            <ul className="flex items-center justify-between pt-4 text-sm">
-
-              <li>Next.js</li><span className="text-purple-600 text-xl ml-1">|</span>
-              <li>Tailwind</li><span className="text-purple-600 text-xl ml-1">|</span>
-              <li>JWT</li><span className="text-purple-600 text-xl ml-1">|</span>
-              <li>MySQL</li><span className="text-purple-600 text-xl ml-1">|</span>
-              <a href="https://github.com/OdaGuzman/WasabiBytes">
-                <Image
-                  src="/github-icon.png"
-                  alt="Github Repo"
-                  className="h-7 ml-auto"
-                  width={28}
-                  height={28}
-                />
-              </a>
-
-            </ul>
-          </div>
-        </div> */}
+        </div>
       </div>
 
       <div className="relative">
